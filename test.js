65789dfeb4b84439b1eec66fe5d6347aed8469a3
Ankush2107 <ankush.kumar210701@gmail.com>
2023-09-02 23:12:35 +0530

    module

---
+++ test.js
@@ -0,0 +1,10 @@
+const calculator = require('./calculator')
+
+
+calculator.addition(7, 10)
+calculator.subtraction(10, 1)
+calculator.multiplication(7, 7)
+calculator.division(15, 3)
+
+// let age = 25
+// console.log(name);
