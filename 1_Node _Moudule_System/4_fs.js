ceacd0c32374435fbc76f07554a51a81dec02b22
Ankush2107 <ankush.kumar210701@gmail.com>
2023-09-03 11:47:43 +0530

    started npm

--- 1_Node _Moudule_System/4_fs.js
+++ 1_Node _Moudule_System/4_fs.js
@@ -1,4 +1,7 @@
-// files
+// Files : 
+/*
+If the file name that is passed doesn't exists a new file will be created with its name and the data will be written on that file.
+*/ 
 
 const fs = require('fs')
 
@@ -11,9 +14,9 @@ const fs = require('fs')
 
 // writing in a file
 
-fs.writeFileSync('f2.txt' , 'This is file 2')
+// fs.writeFileSync('f2.txt' , 'This is file 2')
 
-console.log('File has been written');
+// console.log('File has been written');
 
 // append a file 
 
@@ -26,3 +29,29 @@ console.log('File has been written');
 // fs.unlinkSync('f2.txt')
 // console.log('File has been deleted');
 
+
+
+// Directories
+
+// create a directory
+
+// fs.mkdirSync('mydirectory')
+
+// check the content inside of the directory
+
+// let folderPath = 'C:\Users\dell\Desktop\Scaler_Nodejs\mydirectory'
+// let folderContent = fs.readdirSync(folderPath)
+// console.log(folderContent);
+
+
+
+// check whether a directory exist or not
+
+// let doesExist = fs.existsSync('mydirectory')
+// console.log(doesExist);
+
+
+// remove directory
+
+fs.rmdirSync('mydirectory')
+console.log('File has been delected');
