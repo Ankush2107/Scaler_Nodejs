be437d64fb4d97d1e0f2af7f13497c9bf986d38d
Ankush2107 <ankush.kumar210701@gmail.com>
2023-09-10 23:07:24 +0530

    started mongodb

---
+++ 4_MongoDb/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://127.0.0.1/testDatabase")
+.then(() => console.log('Connection is successful'))
+.catch(err => console.error("could not connect to mongodb,", err))
+
+
+// Schema
+const courseSchema = new mongoose.Schema({
+
+    name: String,
+    creator: String,
+    publishedDate: {type: Date, default: Date.now}, 
+    isPublished: Boolean
+
+})
+
+// Model
+const Course = mongoose.model('Course', courseSchema)
+
+async function createCourse() {
+
+    const course = new Course({
+
+        name: 'Java',
+        creator: 'Navin reddy', 
+        isPublished: true
+    
+    })
+    
+    const result = await course.save()
+    console.log(result);
+}
+
+createCourse()
